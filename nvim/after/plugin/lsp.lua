47d9ac907a84f3d317442f4b229bcb9a5f179915
Joel Chantrell <joelchantrell@protonmail.com>
2023-03-30 08:26:14 +1030

    added prettier and gitsigns

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -7,20 +7,18 @@ lsp.ensure_installed({
   'rust_analyzer',
 })
 
--- Fix Undefined global 'vim'
 lsp.configure('lua_ls', {
   settings = {
     Lua = {
       diagnostics = {
-        globals = {'vim'}
+        globals = { 'vim' }
       }
     }
   }
 })
 
-
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -45,8 +43,22 @@ lsp.set_preferences({
   }
 })
 
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+local lsp_format_on_save = function(bufnr)
+  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    group = augroup,
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format()
+    end,
+  })
+end
+
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
+
+  lsp_format_on_save(bufnr)
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -58,10 +70,38 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "<leader>ff", "<cmd>lua vim.lsp.buf.format()<CR>", { noremap = true })
 end)
+
 
 lsp.setup()
 
 vim.diagnostic.config({
   virtual_text = true
+})
+
+
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+  return
+end
+
+local formatting = null_ls.builtins.formatting
+
+null_ls.setup({
+  debug = false,
+  sources = {
+    formatting.prettier.with({
+      extra_args = {
+        "--print-width 120",
+        "--tab-width 2",
+        "--arrow-parens always",
+        "--end-of-line lf",
+        "--trailing-comma all",
+        "--single-quote",
+        "--jsx-single-quote"
+      }
+    }),
+    --    formatting.stylua,
+  },
 })
