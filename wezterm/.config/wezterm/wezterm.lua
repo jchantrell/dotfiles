060ed8dd12563222ec3bd49d6d0f91aadde6167b
Joel <joelchantrell@protonmail.com>
2024-03-10 16:50:47 +1030

    organise wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -37,7 +37,47 @@ wezterm.on("SplitHorizontalCurrentWorkingDirectory", function(window, pane)
 	)
 end)
 
-local dimmer = { brightness = 0.2 }
+local keys = {
+	{ action = wezterm.action.CopyTo("Clipboard"), mods = "CTRL|SHIFT", key = "C" },
+	{ action = wezterm.action.DecreaseFontSize, mods = "CTRL", key = "-" },
+	{ action = wezterm.action.IncreaseFontSize, mods = "CTRL", key = "=" },
+	{ action = wezterm.action.Nop, mods = "ALT", key = "Enter" },
+	{ action = wezterm.action.PasteFrom("Clipboard"), mods = "CTRL|SHIFT", key = "V" },
+	{ action = wezterm.action.ResetFontSize, mods = "CTRL", key = "0" },
+	{ action = wezterm.action.ToggleFullScreen, key = "F11" },
+	{ action = wezterm.action.ShowDebugOverlay, key = "L", mods = "CTRL" },
+	{ action = wezterm.action.ShowLauncher, key = "Space", mods = "CTRL" },
+	{ action = wezterm.action.CloseCurrentPane({ confirm = false }), key = "x", mods = "ALT|SHIFT" },
+	{ action = wezterm.action.CloseCurrentTab({ confirm = false }), key = "x", mods = "CTRL|SHIFT" },
+	{
+		action = wezterm.action({ EmitEvent = "SpawnCommandInNewWindowInCurrentWorkingDirectory" }),
+		key = "n",
+		mods = "CTRL|SHIFT",
+	},
+	{
+		action = wezterm.action({ EmitEvent = "SpawnCommandInNewTabInCurrentWorkingDirectory" }),
+		key = "t",
+		mods = "CTRL|SHIFT",
+	},
+	{
+		action = wezterm.action({ EmitEvent = "SplitVerticalInCurrentWorkingDirectory" }),
+		key = "_",
+		mods = "ALT|SHIFT",
+	},
+	{
+		action = wezterm.action({ EmitEvent = "SplitHorizontalCurrentWorkingDirectory" }),
+		key = "+",
+		mods = "ALT|SHIFT",
+	},
+	{ key = "h", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "j", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "k", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "l", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	{ key = "H", mods = "ALT|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+	{ key = "J", mods = "ALT|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+	{ key = "K", mods = "ALT|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+	{ key = "L", mods = "ALT|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+}
 
 local config = {
 	adjust_window_size_when_changing_font_size = false,
@@ -48,54 +88,13 @@ local config = {
 	font_size = 16,
 	force_reverse_video_cursor = true,
 	hide_tab_bar_if_only_one_tab = true,
-
-	keys = {
-		{ action = wezterm.action.CopyTo("Clipboard"), mods = "CTRL|SHIFT", key = "C" },
-		{ action = wezterm.action.DecreaseFontSize, mods = "CTRL", key = "-" },
-		{ action = wezterm.action.IncreaseFontSize, mods = "CTRL", key = "=" },
-		{ action = wezterm.action.Nop, mods = "ALT", key = "Enter" },
-		{ action = wezterm.action.PasteFrom("Clipboard"), mods = "CTRL|SHIFT", key = "V" },
-		{ action = wezterm.action.ResetFontSize, mods = "CTRL", key = "0" },
-		{ action = wezterm.action.ToggleFullScreen, key = "F11" },
-		{ action = wezterm.action.ShowDebugOverlay, key = "L", mods = "CTRL" },
-		{ action = wezterm.action.ShowLauncher, key = "Space", mods = "CTRL" },
-		{ action = wezterm.action.CloseCurrentPane({ confirm = false }), key = "x", mods = "ALT|SHIFT" },
-		{ action = wezterm.action.CloseCurrentTab({ confirm = false }), key = "x", mods = "CTRL|SHIFT" },
-		{
-			action = wezterm.action({ EmitEvent = "SpawnCommandInNewWindowInCurrentWorkingDirectory" }),
-			key = "n",
-			mods = "CTRL|SHIFT",
-		},
-		{
-			action = wezterm.action({ EmitEvent = "SpawnCommandInNewTabInCurrentWorkingDirectory" }),
-			key = "t",
-			mods = "CTRL|SHIFT",
-		},
-		{
-			action = wezterm.action({ EmitEvent = "SplitVerticalInCurrentWorkingDirectory" }),
-			key = "_",
-			mods = "ALT|SHIFT",
-		},
-		{
-			action = wezterm.action({ EmitEvent = "SplitHorizontalCurrentWorkingDirectory" }),
-			key = "+",
-			mods = "ALT|SHIFT",
-		},
-		{ key = "h", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-		{ key = "j", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-		{ key = "k", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-		{ key = "l", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-		{ key = "H", mods = "ALT|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
-		{ key = "J", mods = "ALT|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
-		{ key = "K", mods = "ALT|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
-		{ key = "L", mods = "ALT|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
-	},
+	keys = keys,
 	background = {
 		{
 			source = {
 				File = "/home/joel/pics/bg.jpeg",
 			},
-			hsb = dimmer,
+			hsb = { brightness = 0.2 },
 		},
 	},
 	scrollback_lines = 10000,
@@ -103,7 +102,6 @@ local config = {
 	use_dead_keys = false,
 	unicode_version = 14,
 	window_close_confirmation = "NeverPrompt",
-	window_background_opacity = 1,
 	window_padding = {
 		left = 0,
 		right = 0,
